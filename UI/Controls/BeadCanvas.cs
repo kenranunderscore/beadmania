7c8b6307e11d2773a7a31790bb0d672664b06694
InvisiblePanda <jbmaier@web.de>
2017-01-16 22:48:04 +0100

    Toggleable grid lines

--- UI/Controls/BeadCanvas.cs
+++ UI/Controls/BeadCanvas.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Shapes;
 
@@ -8,6 +10,8 @@ namespace beadmania.UI.Controls
 {
     internal class BeadCanvas : Canvas
     {
+        private int pixelSize = 10;
+
         private static DependencyProperty ImageSourceProp =
             DependencyProperty.Register(
                 nameof(ImageSource),
@@ -15,10 +19,23 @@ namespace beadmania.UI.Controls
                 typeof(BeadCanvas),
                 new PropertyMetadata(ImageSourceChanged));
 
+        private static DependencyProperty ShowGridProp =
+            DependencyProperty.Register(
+                nameof(ShowGrid),
+                typeof(bool),
+                typeof(BeadCanvas),
+                new PropertyMetadata(ShowGridChanged));
+
         public System.Drawing.Bitmap ImageSource
         {
             get { return (System.Drawing.Bitmap)GetValue(ImageSourceProp); }
             set { SetValue(ImageSourceProp, value); }
+        }
+
+        public bool ShowGrid
+        {
+            get { return (bool)GetValue(ShowGridProp); }
+            set { SetValue(ShowGridProp, value); }
         }
 
         private static void ImageSourceChanged(DependencyObject obj, DependencyPropertyChangedEventArgs e)
@@ -27,9 +44,54 @@ namespace beadmania.UI.Controls
             target.Draw();
         }
 
+        private static void ShowGridChanged(DependencyObject obj, DependencyPropertyChangedEventArgs e)
+        {
+            var target = (BeadCanvas)obj;
+            target.DrawGrid();
+        }
+
+        private void DrawGrid()
+        {
+            var gridLines = Children.OfType<Line>().ToList();
+            if (!ShowGrid)
+            {
+                foreach (var line in gridLines)
+                {
+                    Children.Remove(line);
+                }
+            }
+            else
+            {
+                for (int x = 0; x < ImageSource.Width; ++x)
+                {
+                    for (int y = 0; y < ImageSource.Height; ++y)
+                    {
+                        Line lv = new Line();
+                        lv.X1 = (x + 1) * pixelSize;
+                        lv.Y1 = 0;
+                        lv.X2 = lv.X1;
+                        lv.Y2 = pixelSize * ImageSource.Height;
+                        lv.Stroke = Brushes.DarkGray;
+                        lv.StrokeThickness = 0.2d;
+
+                        Line lh = new Line();
+                        lh.X1 = 0;
+                        lh.Y1 = (y + 1) * pixelSize;
+                        lh.X2 = pixelSize * ImageSource.Width;
+                        lh.Y2 = lh.Y1;
+                        lh.Stroke = Brushes.DarkGray;
+                        lh.StrokeThickness = 0.2d;
+
+                        Children.Add(lv);
+                        Children.Add(lh);
+                    }
+                }
+            }
+        }
+
         private void Draw()
         {
-            const int pixelSize = 10;
+            Children.Clear();
             for (int x = 0; x < ImageSource.Width; ++x)
             {
                 for (int y = 0; y < ImageSource.Height; ++y)
@@ -44,6 +106,18 @@ namespace beadmania.UI.Controls
                     SetTop(rect, y * pixelSize);
                 }
             }
+
+            DrawGrid();
+        }
+
+        protected override void OnMouseWheel(MouseWheelEventArgs e)
+        {
+            if (e.Delta == 0)
+                return;
+
+            double scaling = e.Delta > 0 ? 2d : 0.5d;
+            pixelSize = (int)(pixelSize * scaling);
+            Draw();
         }
     }
 }
--- UI/ViewModels/MainViewModel.cs
+++ UI/ViewModels/MainViewModel.cs
@@ -5,16 +5,18 @@ namespace beadmania.UI.ViewModels
 {
     internal class MainViewModel : ViewModel
     {
-        public MainViewModel()
-        {
-            Bitmap = (Bitmap)Image.FromFile(@"d:\soh.png");
-        }
-
-        private Bitmap bitmap;
+        private Bitmap bitmap = (Bitmap)Image.FromFile(@"d:\soh.png");
         public Bitmap Bitmap
         {
             get { return bitmap; }
             set { SetProperty(ref bitmap, value); }
+        }
+
+        private bool showGrid = true;
+        public bool ShowGrid
+        {
+            get { return showGrid; }
+            set { SetProperty(ref showGrid, value); }
         }
     }
 }
