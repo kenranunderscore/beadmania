e786d2190038449320697f07cb27eb8783d4a1ac
InvisiblePanda <jbmaier@web.de>
2017-01-21 15:09:09 +0100

    Logic internals made visible to Logic.UnitTests

--- Logic/Properties/AssemblyInfo.cs
+++ Logic/Properties/AssemblyInfo.cs
@@ -18,6 +18,7 @@ using System.Runtime.InteropServices;
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
+[assembly: InternalsVisibleTo("beadmania.Logic.UnitTests")]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("822f7867-7157-46b3-9827-19891da5548d")]
