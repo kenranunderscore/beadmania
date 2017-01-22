d155dd8c2e6c2077a01af35fd143f8292e82ea5f
InvisiblePanda <jbmaier@web.de>
2017-01-22 10:16:38 +0100

    Added simple 3D vector class

---
+++ Logic.UnitTests/Math/Vector3DTest.cs
@@ -0,0 +1,29 @@
+﻿using beadmania.Logic.Math;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace beadmania.Logic.UnitTests.Math
+{
+    [TestClass]
+    public class Vector3DTest
+    {
+        private Vector3D v = new Vector3D(1d, 2d, 3.5d);
+
+        [TestMethod]
+        public void XTest()
+        {
+            Assert.AreEqual(1d, v.X);
+        }
+
+        [TestMethod]
+        public void YTest()
+        {
+            Assert.AreEqual(2d, v.Y);
+        }
+
+        [TestMethod]
+        public void ZTest()
+        {
+            Assert.AreEqual(3.5d, v.Z);
+        }
+    }
+}
---
+++ Logic/Math/Vector3D.cs
@@ -0,0 +1,19 @@
+﻿namespace beadmania.Logic.Math
+{
+    public class Vector3D
+    {
+        private const int Dimensions = 3;
+        private readonly double[] points = new double[Dimensions];
+
+        public Vector3D(double x, double y, double z)
+        {
+            points[0] = x;
+            points[1] = y;
+            points[2] = z;
+        }
+
+        public double X => points[0];
+        public double Y => points[1];
+        public double Z => points[2];
+    }
+}
