29025e559393a837247d3b4babea26ef2d427783
InvisiblePanda <jbmaier@web.de>
2017-01-22 10:47:33 +0100

    ToString returns human readable vector representation

--- Logic.UnitTests/Math/Vector3DTest.cs
+++ Logic.UnitTests/Math/Vector3DTest.cs
@@ -6,24 +6,30 @@ namespace beadmania.Logic.UnitTests.Math
     [TestClass]
     public class Vector3DTest
     {
-        private Vector3D v = new Vector3D(1d, 2d, 3.5d);
+        private Vector3D v1 = new Vector3D(1d, 2d, 3.5d);
 
         [TestMethod]
-        public void XTest()
+        public void X_returns_first_coordinate()
         {
-            Assert.AreEqual(1d, v.X);
+            Assert.AreEqual(1d, v1.X);
         }
 
         [TestMethod]
-        public void YTest()
+        public void Y_returns_second_coordinate()
         {
-            Assert.AreEqual(2d, v.Y);
+            Assert.AreEqual(2d, v1.Y);
         }
 
         [TestMethod]
-        public void ZTest()
+        public void Z_returns_third_coordinate()
         {
-            Assert.AreEqual(3.5d, v.Z);
+            Assert.AreEqual(3.5d, v1.Z);
+        }
+
+        [TestMethod]
+        public void ToString_returns_human_readable_representation()
+        {
+            Assert.AreEqual("x=1;y=2;z=3.5", v1.ToString());
         }
     }
 }
--- Logic/Math/Vector3D.cs
+++ Logic/Math/Vector3D.cs
@@ -1,8 +1,11 @@
-﻿namespace beadmania.Logic.Math
+﻿using System.Globalization;
+
+namespace beadmania.Logic.Math
 {
     public class Vector3D
     {
         private const int Dimensions = 3;
+        private const string RepresentationMask = "x={0};y={1};z={2}";
         private readonly double[] points = new double[Dimensions];
 
         public Vector3D(double x, double y, double z)
@@ -15,5 +18,10 @@
         public double X => points[0];
         public double Y => points[1];
         public double Z => points[2];
+
+        public override string ToString()
+        {
+            return string.Format(CultureInfo.InvariantCulture, RepresentationMask, X, Y, Z);
+        }
     }
 }
