499dda2292a8184a39220275df76942aed67e673
InvisiblePanda <jbmaier@web.de>
2017-01-27 19:21:00 +0100

    Removed unused usings

--- Logic/Delta/DeltaE94Distance.cs
+++ Logic/Delta/DeltaE94Distance.cs
@@ -1,9 +1,4 @@
 ﻿using beadmania.Logic.ColorVectors;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace beadmania.Logic.Delta
 {
