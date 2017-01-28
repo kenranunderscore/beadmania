459a74130c10388ca02fed945fd44dac055d65b0
InvisiblePanda <jbmaier@web.de>
2017-01-28 21:33:32 +0100

    Created BeatPalette

---
+++ Logic.UnitTests/Model/BeadPaletteTest.cs
@@ -0,0 +1,25 @@
+﻿using beadmania.Logic.Model;
+using NUnit.Framework;
+
+namespace beadmania.Logic.UnitTests.Model
+{
+    [TestFixture]
+    public class BeadPaletteTest
+    {
+        [Test]
+        public void New_palette_is_initially_empty()
+        {
+            BeadPalette palette = new BeadPalette();
+            Assert.That(palette.Beads, Is.Empty);
+        }
+
+        [Test]
+        public void Added_beads_are_contained_in_the_palette()
+        {
+            BeadPalette palette = new BeadPalette();
+            Bead bead = new Bead();
+            palette.Add(bead);
+            Assert.That(palette.Beads, Contains.Item(bead));
+        }
+    }
+}
---
+++ Logic/Model/BeadPalette.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace beadmania.Logic.Model
+{
+    public class BeadPalette
+    {
+        private readonly HashSet<Bead> beads = new HashSet<Bead>();
+
+        public IEnumerable<Bead> Beads => beads;
+
+        public void Add(Bead bead)
+        {
+            beads.Add(bead);
+        }
+    }
+}
