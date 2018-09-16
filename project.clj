2f3c0a0761e3f0ef4b45eea034dd9366f4e0443c
kenranunderscore <johb.maier@gmail.com>
2018-09-16 14:15:59 +0200

    Add production build

--- project.clj
+++ project.clj
@@ -6,6 +6,8 @@
 
   :min-lein-version "2.7.1"
 
+  :auto-clean false
+
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.10.339"]
                  [ring "1.7.0-RC2"]
@@ -13,15 +15,28 @@
                  [reacl "2.0.1"]
                  [compojure "1.6.1"]]
 
+  :plugins [[lein-cljsbuild "1.1.7"]]
+
   :source-paths ["src"]
 
   :aliases {"fig"       ["trampoline" "run" "-m" "figwheel.main"]
             "fig:build" ["trampoline" "run" "-m" "figwheel.main" "-b" "dev" "-r"]
             "fig:min"   ["run" "-m" "figwheel.main" "-O" "advanced" "-bo" "dev"]
-            "fig:test"  ["run" "-m" "figwheel.main" "-co" "test.cljs.edn" "-m" beadmania.test-runner]}
+            "fig:test"  ["run" "-m" "figwheel.main" "-co" "test.cljs.edn" "-m" beadmania.test-runner]
+            "prod"      ["do" "clean" ["cljsbuild" "once"] "uberjar"]}
+
+  :main ^:skip-aot beadmania.server
+
+  :cljsbuild {:builds
+              [{:source-paths ["src"]
+                :compiler {:main beadmania.client.core
+                           :output-to "resources/public/cljs-out/dev-main.js"
+                           :optimizations :simple
+                           :pretty-print false}}]}
 
   :profiles {:dev {:dependencies [[com.bhauman/figwheel-main "0.1.9"]
                                   [com.bhauman/rebel-readline-cljs "0.1.4"]]
                    :resource-paths ["target"]
                    ;; need to add the compliled assets to the :clean-targets
-                   :clean-targets ^{:protect false} ["target"]}})
+                   :clean-targets ^{:protect false} ["target"]}
+             :uberjar {:aot :all}})
--- src/beadmania/server.clj
+++ src/beadmania/server.clj
@@ -1,7 +1,9 @@
 (ns beadmania.server
+  (:gen-class)
   (:require [compojure.core :as compojure]
             [compojure.route :as route]
             [compojure.handler :as handler]
+            [ring.adapter.jetty :as jetty]
             [hiccup.page :as h]))
 
 (defn script-file
@@ -19,7 +21,13 @@
 
 (compojure/defroutes main-routes
   (compojure/GET "/" [] index)
+  (route/resources "/")
   (route/not-found "Page could not be found"))
 
 (def ring-handler
   (handler/site main-routes))
+
+(defn -main
+  [& args]
+  (let [port (Integer/parseInt (get (System/getenv) "port" "9500"))]
+    (jetty/run-jetty ring-handler {:port port})))
