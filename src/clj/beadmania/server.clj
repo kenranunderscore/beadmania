434d92b01c9e13c50808d5ff72aa77355394a80c
kenranunderscore <johb.maier@gmail.com>
2018-10-21 15:31:44 +0200

    Add bootstrap framework

--- src/clj/beadmania/server.clj
+++ src/clj/beadmania/server.clj
@@ -6,14 +6,40 @@
             [ring.adapter.jetty :as jetty]
             [hiccup.page :as h]))
 
+(defn include-stylesheet
+  [href & [integrity crossorigin]]
+  [:link {:rel "stylesheet"
+          :href href
+          :integrity integrity
+          :crossorigin crossorigin}])
+
+(defn include-js
+  [src & [integrity crossorigin]]
+  [:script {:type "text/javascript"
+            :src src
+            :integrity integrity
+            :crossorigin crossorigin}])
+
 (def index
   (h/html5
    [:html
     [:head {}
-     [:title "beadmania"]]
+     [:title "beadmania"]
+     (include-stylesheet "https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+                         "sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
+                         "anonymous")]
     [:body {}
      [:div {:id "root"}]
-     (h/include-js "cljs-out/dev-main.js")]]))
+     (include-js "https://code.jquery.com/jquery-3.3.1.slim.min.js"
+                 "sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+                 "anonymous")
+     (include-js "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+                 "sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+                 "anonymous")
+     (include-js "https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+                 "sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+                 "anonymous")
+     (include-js "cljs-out/dev-main.js")]]))
 
 (compojure/defroutes main-routes
   (compojure/GET "/" [] index)
