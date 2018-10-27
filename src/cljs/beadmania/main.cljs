be2a431b962d13960d924be4d9818352ce3fb33f
kenranunderscore <johb.maier@gmail.com>
2018-10-27 10:49:25 +0200

    Add file upload

--- src/cljs/beadmania/main.cljs
+++ src/cljs/beadmania/main.cljs
@@ -2,6 +2,25 @@
   (:require [reacl2.core :as reacl :include-macros true]
             [reacl2.dom :as dom :include-macros true]))
 
+(defrecord UploadFile [file])
+
+(reacl/defclass file-chooser this file []
+  render
+  (dom/form
+   (dom/input
+    {:type "file"
+     :class "form-control-file"
+     :accept "image/png, image/bmp, image/jpeg"
+     :onchange (fn [e]
+                 (let [files (.. e -target -files)]
+                   (when (pos? (alength files))
+                     (reacl/send-message! this (->UploadFile (aget files 0))))))}))
+  handle-message
+  (fn [msg]
+    (cond
+      (instance? UploadFile msg)
+      (.log js/console (:file msg)))))
+
 (reacl/defclass beadmania this []
   render
   (dom/div
@@ -9,8 +28,9 @@
     {:class "navbar navbar-expand-lg navbar-dark bg-dark"}
     (dom/a
      {:class "navbar-brand"
-            :href "#"}
-     "beadmania")))
+      :href "#"}
+     "beadmania"))
+   (file-chooser nil))
 
   handle-message
   #(reacl/return))
