22bcf4c6b53a610e3d8644c470c3032df9a5530d
kenranunderscore <johb.maier@gmail.com>
2018-11-18 12:12:26 +0100

    Remove figwheel-main and fully switch to shadow-cljs

--- src/beadmania/core.cljs
+++ src/beadmania/core.cljs
@@ -3,14 +3,6 @@
 
 (enable-console-print!)
 
-;; define your app data so that it doesn't get over-written on reload
-
-(defn on-js-reload []
-  ;; optionally touch your app-state to force rerendering depending on
-  ;; your application
-  ;; (swap! app-state update-in [:__figwheel_counter] inc)
-  )
-
 (defn init!
   []
   (println "Successfully loaded"))
