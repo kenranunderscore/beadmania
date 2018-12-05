448faee7cb8b51057c0aac3ae423bf2a50125aec
kenranunderscore <johb.maier@gmail.com>
2018-12-05 21:32:15 +0100

    Add quickcheck tests

--- test/beadmania/image_operations_test.clj
+++ test/beadmania/image_operations_test.clj
@@ -1,6 +1,10 @@
 (ns beadmania.image-operations-test
-  (:require [clojure.test :refer [deftest is testing]]
-            [beadmania.image-operations :as image-op]))
+  (:require [beadmania.image-operations :as image-op]
+            [clojure.test :refer [deftest is testing]]
+            [clojure.test.check :as check]
+            [clojure.test.check.generators :as gen]
+            [clojure.test.check.properties :as prop]
+            [clojure.test.check.clojure-test :refer [defspec]]))
 
 (deftest color-conversions-are-inverse
   (is (= [10 20 30 40]
@@ -10,4 +14,19 @@
 
 (deftest t-color->rgb-color
   (is (= [253 153 153]
-         (image-op/color->rgb-color [0.4 250 0 0]))))
+         (image-op/color->rgb-color [102 250 0 0]))))
+
+(defspec argb->color-is-left-inverse-to-color->argb
+  100
+  (prop/for-all [v (gen/vector (gen/large-integer* {:min 0 :max 255}) 4)]
+                (= v
+                   (->> v
+                        image-op/color->argb
+                        image-op/argb->color))))
+
+(defspec color->rgb-color-produces-sane-values
+  100
+  (prop/for-all [v (gen/vector (gen/large-integer* {:min 0 :max 255}) 3)]
+                (not-any? #(or (neg? %)
+                               (> % 255))
+                          (image-op/color->rgb-color v))))
